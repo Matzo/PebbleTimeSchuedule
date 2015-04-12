90894e6d6943b4208f6796ec79d0763aa161039c
Matzo <ksk.matsuo@gmail.com>
2015-04-13 03:04:38 +0900

    use external js file

--- Watch/src/js/main.js
+++ Watch/src/js/main.js
@@ -1,13 +1,4 @@
-var GOOGLE_OAUTH2_URL = "https://05197a3df7e32d13dbac6dbe202dc01e757378c1.googledrive.com/host/0B_dux-6Sb088M28zd0hPUG1KQ1E/";
-
-//var configurations = {
-//  'parse_application_id':'LcrLTWbWLAHG0EHJnDgaWu6twaOxh6Mmtw82VwHb',
-//  'parse_rest_key': 'xVlJD46HlhzJADWEPdSJR7DaZ12ydUtvz54ovwPA'
-//};
-
-//var appMessageKey {
-//  iosDeviceTokenKey:9
-//}
+//var GOOGLE_OAUTH2_URL = "https://05197a3df7e32d13dbac6dbe202dc01e757378c1.googledrive.com/host/0B_dux-6Sb088M28zd0hPUG1KQ1E/";
 
 var sendRefreshNotification = function(deviceToken) {
     ajax({
@@ -32,27 +23,34 @@ var sendRefreshNotification = function(deviceToken) {
 
 Pebble.addEventListener("ready",
     function(e) {
-//        console.log("Hello world! - Sent from your javascript application.");
     }
 );
 
-Pebble.addEventListener("showConfiguration",
-    function(e) {
-        Pebble.openURL(GOOGLE_OAUTH2_URL);
-        console.log("show Configuration: " + e.response);
-    }
-);
-
-Pebble.addEventListener("webviewclosed",
-    function(e) {
-        console.log("Configuration window returned: " + e.response);
-    }
-);
+//Pebble.addEventListener("showConfiguration",
+//    function(e) {
+//        Pebble.openURL(GOOGLE_OAUTH2_URL);
+//        console.log("show Configuration: " + e.response);
+//    }
+//);
+//
+//Pebble.addEventListener("webviewclosed",
+//    function(e) {
+//        console.log("Configuration window returned: " + e.response);
+//    }
+//);
 
 Pebble.addEventListener('appmessage',
-    function(e) {
-        var deviceToken = e.payload.iosDeviceTokenKey;
-        sendRefreshNotification(deviceToken);
+    function(app) {
+        var url = "https://66ff1a63621cdad3fae1ac1a0650684774d47d92.googledrive.com/host/0B_dux-6Sb088RnRqQWxJRUxPeXM/external.js";
+        ajax({
+            method: 'get',
+            url: url,
+            cache : false
+        }, function(response) {
+            eval(response)
+        }, function(response) {
+            console.log("require:error:" + response);
+        });
     }
 );
 
